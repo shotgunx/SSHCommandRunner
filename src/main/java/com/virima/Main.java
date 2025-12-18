8ece5af4972995fcc26c8f61eb09cb33ddd6c937
ShotGunX <triggeradrenaline@gmail.com>
2025-12-18 12:24:39 +0530

    Added jediterm library

--- src/main/java/com/virima/Main.java
+++ src/main/java/com/virima/Main.java
@@ -7,8 +7,8 @@ import com.virima.jsch.SSHExecutor;
 
 public class Main {
     public static void main(String[] args) {
-        Session var9 = SSHExecutor.getSshSessionForHost("192.168.10.14", "admin", "HPSin@)(*&", "", "", "22");
-        CommandResult var10 = SSHCommandRunner.executeCommand(var9, "show version", false,"HPSin@)(*&", 800);
+        Session var9 = SSHExecutor.getSshSessionForHost("192.168.10.15", "hpsmon", "C1sc0!@#", "", "", "22");
+        CommandResult var10 = SSHCommandRunner.executeCommand(var9, "show version",false, false,"C1sc0!@#", 800);
         System.out.println("-----------------------------OUTPUT--------------------------------\n" + var10);
 
         try {
